577f94ec2f66746ac51ade5ce9c8581f19c6dd4e
elecoant <lct.etienne2@gmail.com>
2023-12-14 09:35:42 +0100

    start moving the car + put some tree

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,23 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CarController : MonoBehaviour
 {
     bool isRunning = false;
 
-    // Start is called before the first frame update
+    // Rigidbody of the player.
+    private Rigidbody rb;
+
+    // Movement along X and Y axes.
+    private float movementX;
+    private float movementY;
+
+    // Speed at which the player moves.
+    public float speed = 0;
+
+    
+
+
     void Start()
     {
-        
+        // Get and store the Rigidbody component attached to the player.
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    // This function is called when a move input is detected.
+    void OnMove(InputValue movementValue)
     {
-        if(isRunning)
-        {
+        // Convert the input value into a Vector2 for movement.
+        Vector2 movementVector = movementValue.Get<Vector2>();
 
-        }
+        // Store the X and Y components of the movement.
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    // FixedUpdate is called once per fixed frame-rate frame.
+    private void FixedUpdate()
+    {
+        // Create a 3D movement vector using the X and Y inputs.
+        Vector3 movement = new(movementX, 0.0f, movementY);
+
+        // Apply force to the Rigidbody to move the player.
+        rb.AddForce(movement * speed);
     }
 }
